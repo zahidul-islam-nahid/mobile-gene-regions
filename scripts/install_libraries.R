8fc47f73640a3beb1c84761330c49511b7640ff9
zahidul-islam-nahid <106141355+zahidul-islam-nahid@users.noreply.github.com>
2025-10-08 09:37:40 +0600

    Update install_libraries.R
    R libraries will be installed through micromamba, if not will be installed from CRAN

--- scripts/install_libraries.R
+++ scripts/install_libraries.R
@@ -1,12 +1,13 @@
-regular.packages = c("ape", "cowplot", "docopt", "ggdendro", "gridExtra",
-             "gggenes", "ggrepel", "reshape2", "RColorBrewer", "vegan")
+regular.packages <- c("ape","cowplot","docopt","ggdendro","gridExtra",
+                      "gggenes","ggrepel","reshape2","RColorBrewer","vegan","dplyr")
 
-for (p in regular.packages){
-  if (!require(p, quietly = TRUE))
-    install.packages(p, repos = "http://cran.us.r-project.org")
+for (p in regular.packages) {
+  if (!require(p, character.only = TRUE, quietly = TRUE)) {
+    install.packages(p, repos = "https://cran.r-project.org")
+  }
 }
 
-if (!require("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install("ggtree")
-
+if (!require("ggtree", character.only = TRUE, quietly = TRUE)) {
+  if (!require("BiocManager", quietly = TRUE)) install.packages("BiocManager")
+  BiocManager::install("ggtree", ask = FALSE, update = FALSE)
+}
